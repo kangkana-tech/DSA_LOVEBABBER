b95a1c9b0ac48a9b49a4f48594f7f2a1f1dd1389
kangkana-tech <kalitakangkana15@gmail.com>
2023-08-31 17:10:23 +0530

    leetcode_prob_solve

---
+++ leetcodeVAR.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int subtractProductAndSum(int n){
+
+    int prod = 1;
+    int sum = 0;
+    while(n!=0){
+        int digit = n%10;
+        prod = prod*digit;
+        sum = sum + digit;
+        n = n/10;
+    }
+    int answer = prod - sum;
+    cout<<answer<<endl;;
+}
+int main(){
+    int n;
+    cout<<"enter a number : ";
+    cin>> n;
+    subtractProductAndSum(n);
+}
